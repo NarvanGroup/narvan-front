07cffbe9d89bac319963c20f6f02d1a26b642296
a.rezakhani <a.rezakhani@azki.com>
2023-07-27 16:22:34 +0330

    add_initial_structure

---
+++ components/LanguageSwitcher.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+export default function LanguageSwitcher() {
+  const router = useRouter();
+
+  const { locales, locale: activeLocale } = router;
+
+  const otherLocales = locales?.filter((locale: string) => locale !== activeLocale && locale !== 'default');
+
+  return (
+    <span className="text-muted cursor-pointer">
+      {otherLocales?.map((locale: string) => {
+        const { pathname, query, asPath } = router;
+        return (
+          <span key={'locale-' + locale}>
+            <Link href={{ pathname, query }} as={asPath} locale={locale}>
+              <a>{locale === 'en' ? 'English' : locale === 'fa' ? 'فارسی' : null}</a>
+            </Link>
+          </span>
+        );
+      })}
+    </span>
+  );
+}
