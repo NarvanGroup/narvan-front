c45eff1f217e658af611b7baa7616d04cae65eeb
Aida Rezakhani <aidarezakhani@Aidas-MacBook-Air.local>
2023-09-11 17:48:48 +0430

    delete_narvan

--- components/Logo.tsx
+++
@@ -1,5 +0,0 @@
-import Image from 'next/image';
-
-export default function Logo({ ...rest }) {
-  return <Image width={150} height={100} alt="Narvan Logo" src="/Logos/Logo_english_transparent.png" />;
-}
